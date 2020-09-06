0841306119705a7d15e8f7b200c568170547361d
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-06 12:49:56 -0700

    update smallestRange

--- src/com/company/SmallestRange.java
+++ src/com/company/SmallestRange.java
@@ -7,9 +7,59 @@ public class SmallestRange {
         int[][] matrix = new int[][] {{1, 2}, {3, 4}};
         System.out.println(Arrays.toString(s.smallestRange(matrix)));
     }
+
+    // Method 2:
     // Time O(kn * k)
     // Space O(k)
     public int[] smallestRange(int[][] matrix) {
+        int length = 0;
+        PriorityQueue<Solution.Entry> minHeap = new PriorityQueue<Solution.Entry>(14, new Solution.MyComparator());
+        for (int row = 0; row < matrix.length; row++) {
+            length += matrix[row].length;
+            if (matrix[row].length != 0) {
+                minHeap.offer(new Solution.Entry(row, 0, matrix[row][0]));
+            }
+        }
+        int[] result = new int[length];
+        int curr = 0;
+        while (!minHeap.isEmpty()) {
+            Solution.Entry tmp = minHeap.poll();
+            result[curr] = tmp.value;
+            curr++;
+            if (tmp.col + 1 < matrix[tmp.row].length) {
+                tmp.col++;
+                tmp.value = matrix[tmp.row][tmp.col];
+                minHeap.offer(tmp);
+            }
+        }
+        return result;
+    }
+    static class MyComparator implements Comparator<Solution.Entry> {
+        @Override
+        public int compare(Solution.Entry e1, Solution.Entry e2) {
+            if (e1.value == e2.value) {
+                return 0;
+            }
+            return e1.value < e2.value ? -1 : 1;
+        }
+    }
+
+    static class Entry {
+        int row;
+        int col;
+        int value;
+
+        Entry(int row, int col, int value) {
+            this.row = row;
+            this.col = col;
+            this.value = value;
+        }
+    }
+
+    // Method 1:
+    // Time O(kn * k)
+    // Space O(k)
+    public int[] smallestRange2(int[][] matrix) {
         if (matrix == null || matrix.length == 0) {
             return new int[0];
         }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,2 +1,50 @@
 package com.company;
+import java.util.*;
+
+public class Solution {
+    public int[] smallestRange(int[][] matrix) {
+        int length = 0;
+        PriorityQueue<Entry> minHeap = new PriorityQueue<Entry>(14, new MyComparator());
+        for (int row = 0; row < matrix.length; row++) {
+            length += matrix[row].length;
+            if (matrix[row].length != 0) {
+                minHeap.offer(new Entry(row, 0, matrix[row][0]));
+            }
+        }
+        int[] result = new int[length];
+        int curr = 0;
+        while (!minHeap.isEmpty()) {
+            Entry tmp = minHeap.poll();
+            result[curr] = tmp.value;
+            curr++;
+            if (tmp.col + 1 < matrix[tmp.row].length) {
+                tmp.col++;
+                tmp.value = matrix[tmp.row][tmp.col];
+                minHeap.offer(tmp);
+            }
+        }
+        return result;
+    }
+    static class MyComparator implements Comparator<Entry> {
+        @Override
+        public int compare(Entry e1, Entry e2) {
+            if (e1.value == e2.value) {
+                return 0;
+            }
+            return e1.value < e2.value ? -1 : 1;
+        }
+    }
+
+    static class Entry {
+        int row;
+        int col;
+        int value;
+
+        Entry(int row, int col, int value) {
+            this.row = row;
+            this.col = col;
+            this.value = value;
+        }
+    }
+}
 
