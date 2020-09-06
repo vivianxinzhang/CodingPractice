4094981dda958f05357fa662d1de4925002b96e5
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-05 19:44:45 -0700

    update mergeSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,10 @@ public class Main {
 //        MergeKSortedArray m = new MergeKSortedArray();
         SmallestRange s = new SmallestRange();
         MergeSort m = new MergeSort();
-        int[] array = {5, 4};
+        int[] array = {};
         int[][] matrix = new int[][] {{1, 2}, {3, 4}};
         System.out.println(Arrays.toString(m.mergeSort1(array)));
+        System.out.println(Arrays.toString(m.mergeSort2(array)));
+
     }
 }
--- src/com/company/MergeSort.java
+++ src/com/company/MergeSort.java
@@ -1,6 +1,52 @@
 package com.company;
 
 public class MergeSort {
+    // Method 2
+    // Time O(nlogn)
+    // Space O(n)
+    public int[] mergeSort2(int[] array) {
+        if (array == null || array.length <= 1) {
+            return array;
+        }
+        int[] helper = new int[array.length];
+        mergeSort2(array, 0, array.length - 1, helper);
+        return array;
+    }
+
+    private void mergeSort2(int[] array, int left, int right, int[] helper) {
+        // base case
+        if (left == right) {
+            return;
+        }
+        // recursion rule
+        int mid = left + (right - left) / 2;
+        mergeSort2(array, left, mid, helper);
+        mergeSort2(array, mid + 1, right, helper);
+        merge2(array, left, mid, right, helper);
+    }
+
+    private void merge2(int[] array, int left, int mid, int right, int[] helper) {
+        for (int i = left; i <= right; i++) {
+            helper[i] = array[i];
+        }
+        int leftIdx = left, rightIdx = mid + 1, idx = left;
+        while (leftIdx <= mid && rightIdx <= right) {
+            if (helper[leftIdx] < helper[rightIdx]) {
+                array[idx] = helper[leftIdx];
+                leftIdx++;
+            } else {
+                array[idx] = helper[rightIdx];
+                rightIdx++;
+            }
+            idx++;
+        }
+        while (leftIdx <= mid) {
+            array[idx] = helper[leftIdx];
+            leftIdx++;
+            idx++;
+        }
+    }
+
     // Method 1
     // Time O(nlogn)
     // Space O(n)
@@ -8,18 +54,18 @@ public class MergeSort {
         if (array == null || array.length <= 1) {
             return array;
         }
-        return mergeSort(array, 0, array.length - 1);
+        return mergeSort1(array, 0, array.length - 1);
     }
 
-    private int[] mergeSort(int[] array, int left, int right) {
+    private int[] mergeSort1(int[] array, int left, int right) {
         // base case
         if (left == right) {
             return new int[] {array[left]};
         }
         // recursion rule
         int mid = left + (right - left) / 2;
-        int[] leftRes = mergeSort(array, left, mid);
-        int[] rightRes = mergeSort(array, mid + 1, right);
+        int[] leftRes = mergeSort1(array, left, mid);
+        int[] rightRes = mergeSort1(array, mid + 1, right);
         return merge(leftRes, rightRes);
     }
 
