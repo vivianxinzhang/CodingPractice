b4e9af4b257139305962786ad8b921439c33c22a
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-07 13:40:49 -0700

    update StackWithMin

--- src/com/company/StackWithMin.java
+++ src/com/company/StackWithMin.java
@@ -1,40 +1,46 @@
 package com.company;
+
 import java.util.*;
 
 // Method 1:
 public class StackWithMin {
-    private Deque<Integer> stack;
-    private Deque<Integer> minStack;
+    // min used to store elements contribute to min
+    Deque<Integer> min;
+    Deque<Integer> stack;
 
     public StackWithMin() {
-        stack = new LinkedList<Integer>();
-        minStack = new LinkedList<Integer>();
-    }
-
-    public int min() {
-        return minStack.peekFirst();
-    }
-
-    public void push(int value) {
-        stack.offerFirst(value);
-        // when value <= current min value in stack,
-        // need to push the value to minStack.
-        if (value <= minStack.peekFirst()) {
-            minStack.offerFirst(value);
-        }
+        // write your solution here
+        min = new ArrayDeque<>();
+        stack = new ArrayDeque<>();
     }
 
     public int pop() {
-        Integer result = stack.pollFirst();
         // when the popped value is the same as top value of minStack, the value
         // need to be popped from minStack as well.
-        if (minStack.peekFirst().equals(result)) {
-            minStack.pollFirst();
+        if (stack.isEmpty()) {
+            return -1;
         }
-        return result;
+        int tmp = stack.pop();
+        if (tmp == min.peek()) {
+            min.pop();
+        }
+        return tmp;
+    }
+
+    public void push(int element) {
+        // when value <= current min value in stack,
+        // need to push the value to minStack.
+        stack.push(element);
+        if (min.isEmpty() || element <= min.peek()) {
+            min.push(element);
+        }
     }
 
     public int top() {
-        return stack.peekFirst();
+        return stack.isEmpty() ? -1 : stack.peek();
+    }
+
+    public int min() {
+        return min.isEmpty() ? -1 : min.peek();
     }
 }
