f18a7e499993cc4150374a4744b7df33b4624df7
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-09 21:59:31 -0700

    rainbowSort k colors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-//        Solution s = new Solution();
-        ClusterWords1 s1 = new ClusterWords1();
-        ClusterWords2 s2 = new ClusterWords2();
-        List<String> list = new ArrayList<>();
-        list.add("w");
-        list.add("f");
-        list.add("h");
-        list.add("e");
-        System.out.println(list);
-        s1.clusterWords(list);
-        System.out.println(list);
-        list.add("a");
-        System.out.println(list);
-        s1.clusterWords(list);
-        System.out.println(list);
-//        int[] array = {1, 2, 3, 5, 2};
+        Solution s = new Solution();
+        int[] array = {};
+        System.out.println(Arrays.toString(s.rainbowSortIII(array, 5)));
+
 //        int[][] matrix = new int[][] { { 1, 2, 3, 4 }, { 4, 5, 6, 7 }, { 2, 3, 4, 8 } };
 //        System.out.println(Arrays.toString(s.quickSort(array)));
     }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,2 +1,35 @@
+package com.company;
+import java.util.*;
 
+//
+public class Solution {
+    public int[] rainbowSortIII(int[] array, int k) {
+        // Write your solution here
+        if (array == null || array.length == 0) {
+            return array;
+        }
+        int left = 0, right = array.length - 1;
+        int leftColor = 1, rightColor = k;
+        while (leftColor < rightColor) {
+            for (int i = left; i <= right; i++) {
+                if (array[i] == leftColor) {
+                    swap(array, left, i);
+                    left++;
+                } else if (array[i] == rightColor) {
+                    swap(array, i, right);
+                    right--;
+                    i--;
+                }
+            }
+            leftColor++;
+            rightColor--;
+        }
+        return array;
+    }
 
+    public static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
