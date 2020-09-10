a05d54662e87777e486973bc1c18438b2aca5b03
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-10 16:17:45 -0700

    update rainbow sort k colors

--- src/com/company/RainbowSort.java
+++ src/com/company/RainbowSort.java
@@ -52,6 +52,7 @@ public class RainbowSort {
     }
 
     // rainbow sort k colors: Time O(nk) Space O(1)
+    // Implementation 1:
     public int[] rainbowSortIII(int[] array, int k) {
         // Write your solution here
         if (array == null || array.length == 0) {
@@ -72,6 +73,33 @@ public class RainbowSort {
             }
             leftColor++;
             rightColor--;
+        }
+        return array;
+    }
+
+    // rainbow sort k colors: Time O(nk) Space O(k)
+    // Implementation 2:
+    public int[] rainbowSortIV(int[] array, int k) {
+        // Write your solution here
+        if (array == null || array.length == 0) {
+            return array;
+        }
+        int[] idx = new int[k];
+        idx[k - 1] = array.length - 1;
+        // need to check if k - 2 is out of bound
+        // or can treat k == 1 as corner case
+        while (k >= 2 && idx[k - 2] <= idx[k - 1]) {
+            if (array[idx[k - 2]] == k) {
+                swap(array, idx[k - 2], idx[k - 1]);
+                idx[k - 1]--;
+            } else {
+                int distance = k - 1 - array[idx[k - 2]];
+                for (int i = 0; i < distance; i++) {
+                    swap(array, idx[k - 3 - i], idx[k - 2 - i]);
+                    idx[k - 2 - i]++;
+                }
+                idx[k - 2 - distance]++;
+            }
         }
         return array;
     }
