1547ecacb4137ff3b077918d70702ff79262dfc0
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-20 11:57:40 -0700

    test AddTwoNumbers

--- src/com/company/AddTwoNumbers.java
+++ src/com/company/AddTwoNumbers.java
@@ -1,37 +1,68 @@
 package com.company;
 
 public class AddTwoNumbers {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static void main(String[] args) {
+        AddTwoNumbers s = new AddTwoNumbers();
+        ListNode l1 = new ListNode(2);
+        ListNode four = new ListNode(4);
+        l1.next = four;
+        ListNode three = new ListNode(3);
+        four.next = three;
+        ListNode curr1 = l1;
+        while (curr1 != null) {
+            System.out.print(curr1.value);
+            curr1 = curr1.next;
+        }
+        System.out.println();
+        ListNode l2 = new ListNode(5);
+        ListNode six = new ListNode(6);
+        l2.next = six;
+        ListNode four2 = new ListNode(4);
+        six.next = four2;
+        ListNode curr2 = l1;
+        while (curr2 != null) {
+            System.out.print(curr2.value);
+            curr2 = curr2.next;
+        }
+        System.out.println();
+        ListNode curr = s.addTwoNumbers(l1, l2);
+        while (curr != null) {
+            System.out.print(curr.value);
+            curr = curr.next;
+        }
+    }
+
+    public ListNode addTwoNumbersI(ListNode l1, ListNode l2) {
         // Write your solution here
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        int number = 0;
+        ListNode curr = dummy;
+        int num = 0;
         while (l1 != null && l2 != null) {
-            number += l1.value;
+            num += l1.value;
             l1 = l1.next;
-            number += l2.value;
+            num += l2.value;
             l2 = l2.next;
-            tail.next = new ListNode(number % 10);
-            number /= 10;
-            tail = tail.next;
+            curr.next = new ListNode(num % 10);
+            num /= 10;
+            curr = curr.next;
         }
         while (l1 != null) {
-            number += l1.value;
-            tail.next = new ListNode(number % 10);
+            num += l1.value;
+            curr.next = new ListNode(num % 10);
+            num /= 10;
+            curr = curr.next;
             l1 = l1.next;
-            tail = tail.next;
-            number /= 10;
         }
         while (l2 != null) {
-            number += l2.value;
-            tail.next = new ListNode(number % 10);
+            num += l2.value;
+            curr.next = new ListNode(num % 10);
+            num /= 10;
+            curr = curr.next;
             l2 = l2.next;
-            tail = tail.next;
-            number /= 10;
         }
-        while (number != 0) {
-            tail.next = new ListNode(number % 10);
-            number /= 10;
+        while (num != 0) {
+            curr.next = new ListNode(num % 10);
+            num /= 10;
         }
         return dummy.next;
     }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -12,3 +12,39 @@ import java.util.*;
  * }
  */
 
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // Write your solution here
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int num = 0;
+        while (l1 != null && l2 != null) {
+            num += l1.value;
+            l1 = l1.next;
+            num += l2.value;
+            l2 = l2.next;
+            curr.next = new ListNode(num % 10);
+            num /= 10;
+            curr = curr.next;
+        }
+        while (l1 != null) {
+            num += l1.value;
+            curr.next = new ListNode(num % 10);
+            num /= 10;
+            curr = curr.next;
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            num += l2.value;
+            curr.next = new ListNode(num % 10);
+            num /= 10;
+            curr = curr.next;
+            l2 = l2.next;
+        }
+        while (num != 0) {
+            curr.next = new ListNode(num % 10);
+            num /= 10;
+        }
+        return dummy.next;
+    }
+}
