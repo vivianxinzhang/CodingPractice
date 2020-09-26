595c04289003962a9a2a9d737e076929ba2a2e81
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-26 15:14:37 -0700

    add insertBST

---
+++ src/com/company/InsertBST.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class InsertBST {
+    // method 1: recursion
+    // Time O(n) better answer O(h)
+    // Space O(h) worst case O(n)
+    public  TreeNode insert(TreeNode root, int key) {
+        if (root == null) {
+            return new TreeNode(key);
+        }
+        if (key < root.key) {
+            root.left = insert(root.left, key);
+        } else if (key > root.key) {
+            root.right = insert(root.right, key);
+        }
+        return root;
+    }
+
+    // method 2: iterative
+    // Time O(n) better answer O(h)
+    // Space O(1)
+    public  TreeNode insertI(TreeNode root, int key) {
+        if (root == null) {
+            return new TreeNode(key);
+        }
+        TreeNode cur = root;
+        while (cur.key != key) {
+            if (key < cur.key) {
+                if (cur.left == null) {
+                    cur.left = new TreeNode(key);
+                }
+            } else {
+                if (cur.right == null) {
+                    cur.right = new TreeNode(key);
+                }
+                cur = cur.right;
+            }
+        }
+        return root;
+    }
+}
