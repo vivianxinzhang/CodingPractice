8e610e0b816defead5a780a665ec4791243d3874
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-26 15:38:47 -0700

    add DeleteBST

---
+++ src/com/company/DeleteBST.java
@@ -0,0 +1,42 @@
+package com.company;
+
+
+public class DeleteBST {
+    // Time O(h) worst O(n)
+    // Space O(h) worst O(n)
+    public TreeNode delete(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (key == root.key) {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else if (root.right.left == null) {
+                root.right.left = root.left;
+                return root.right;
+            } else {
+                TreeNode newRoot = deleteSmallest(root.right);
+                newRoot.left = root.left;
+                newRoot.right = root.right;
+                return newRoot;
+            }
+        }
+        if (key < root.key) {
+            root.left = delete(root.left, key);
+        } else if (key > root.key) {
+            root.right = delete(root.right, key);
+        }
+        return root;
+    }
+
+    private TreeNode deleteSmallest(TreeNode root) {
+        while (root.left.left != null) {
+            root = root.left;
+        }
+        TreeNode smallest = root.left;
+        root.left = root.left.right;
+        return smallest;
+    }
+}
