cd69973a1678a84f4f029a5e6bafa71fc839df18
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-26 17:03:22 -0700

    update postOrderBST iteration

--- src/com/company/PostOrder.java
+++ src/com/company/PostOrder.java
@@ -27,4 +27,45 @@ public class PostOrder {
         Collections.reverse(result);
         return result;
     }
+
+    // Method 2: check the relation between the current node and the previous node
+    // to determine which direction should go next
+    public List<Integer> postOrderII(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.offerFirst(root);
+        // to record the previous node on the way of DFS so that
+        // we can determine the direction
+        TreeNode prev = null;
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.peekFirst();
+            // if we are going down, either left/right direction
+            if (prev == null || cur == prev.left || cur == prev.right) {
+                // if we can still go down, try to go left first
+                if (cur.left != null) {
+                    stack.offerFirst(cur.left);
+                } else if (cur.right != null) {
+                    stack.offerFirst(cur.right);
+                } else {
+                    // if we can not go either way, meaning cur is a leaf node
+                    stack.pollFirst();
+                    result.add(cur.key);
+                }
+            } else if (prev == cur.right || prev == cur.left && cur.right == null) {
+                // if we are going up from the right side or going up from the left side
+                // but we cannot go right afterwards
+                stack.pollFirst();
+                result.add(cur.key);
+            } else {
+                // otherwise, we are going up from the left side and we can go down
+                // right side
+                stack.offerFirst(cur.right);
+            }
+            prev = cur;
+        }
+        return result;
+    }
 }
