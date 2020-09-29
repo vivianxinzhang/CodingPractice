fea79e766cb480eec1259c6654c49b6dddfdf2ff
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-29 14:56:56 -0700

    add reverse linked list

---
+++ src/com/company/ReverseLinkedList.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class ReverseLinkedList {
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public ListNode reverseI(ListNode head) {
+        // be careful about the base case,
+        // need to make sure the later head.next.next != null
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseI(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,3 +1,18 @@
 package com.company;
 import java.util.*;
 
+// Time O(n)
+// Space O(h) worst O(n)
+public class Solution {
+    public ListNode reverse(ListNode head) {
+        // Write your solution here
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
