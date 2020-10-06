c735a57f657e82333b6aab3a77a9cc8ad44e1b09
Vivian Zhang <vivianxinzhang@gmail.com>
2020-10-05 17:08:57 -0700

    add BinaryTreeDiameter

--- src/com/company/BinaryTreeDiameter.java
+++ src/com/company/BinaryTreeDiameter.java
@@ -1,5 +1,25 @@
 package com.company;
 
 public class BinaryTreeDiameter {
+    // Time O(n)
+    // Space O(h)
+    public int diameter(TreeNode root) {
+        // Write your solution here
+        int[] maximum = new int[1];
+        helper(root, maximum);
+        return maximum[0];
+    }
 
+    private int helper(TreeNode root, int[] maximum) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = helper(root.left, maximum);
+        int rightHeight = helper(root.right, maximum);
+        if (root.left != null && root.right != null) {
+            int currDiameter = leftHeight + rightHeight + 1;
+            maximum[0] = Math.max(maximum[0], currDiameter);
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,7 +1,5 @@
 package com.company;
 import java.util.*;
 
-// Time O(mn)
-// Space O(1)
 
 
