c842fb23d8f89f066f41a775e55af9a513ac51f1
Vivian Zhang <vivianxinzhang@gmail.com>
2020-10-06 20:46:08 -0700

    InvertBinaryTree

--- src/com/company/InvertBinaryTree.java
+++ src/com/company/InvertBinaryTree.java
@@ -4,17 +4,6 @@ public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
         // Write your solution here
         if (root == null) {
-            return root;
-        }
-        TreeNode tmp = root.left;
-        root.left = invertTree(root.right);
-        root.right = invertTree(tmp);
-        return root;
-    }
-
-    public TreeNode invertTreeI(TreeNode root) {
-        // Write your solution here
-        if (root == null) {
             return null;
         }
         TreeNode tmp = root.left;
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,3 +1,17 @@
 package com.company;
 import java.util.*;
 
+public class Solution {
+    // Time O(n)
+    // Space O(height) worst case O(n)
+    public TreeNode invertTree(TreeNode root) {
+        // Write your solution here
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        return root;
+    }
+}
