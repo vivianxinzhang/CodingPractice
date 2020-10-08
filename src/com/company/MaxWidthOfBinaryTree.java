4d591c51d2ecec918a5044c3f61868fa545cabdd
Vivian Zhang <vivianxinzhang@gmail.com>
2020-10-07 17:01:05 -0700

    update iterative MaxWidthOfBinaryTree

--- src/com/company/MaxWidthOfBinaryTree.java
+++ src/com/company/MaxWidthOfBinaryTree.java
@@ -15,6 +15,7 @@ public class MaxWidthOfBinaryTree {
         System.out.println(s.maxWidthOfBinaryTree(one));
     }
 
+    // Recursion
     // Time O(n)
     // Space O(height) worst case O(n)
     public int maxWidthOfBinaryTree(TreeNode root) {
@@ -35,5 +36,43 @@ public class MaxWidthOfBinaryTree {
         int leftWidth = dfs(root.left, level + 1, 2 * id, startID);
         int rightWidth = dfs(root.right, level + 1, 2 * id + 1, startID);
         return Math.max(currWidth, Math.max(leftWidth, rightWidth));
+    }
+
+    // Time O(n)
+    // Space O(n)
+    public int maxWidthOfBinaryTreeI(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        Map<TreeNode, Integer> map = new HashMap<>();
+        map.put(root, 0);
+        int maxWidth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int start = 0;
+            int end = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (i == 0) {
+                    start = map.get(curr);
+                }
+                if (i == size - 1) {
+                    end = map.get(curr);
+                }
+                if (curr.left != null) {
+                    map.put(curr.left, map.get(curr) * 2);
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    map.put(curr.right, map.get(curr) * 2 + 1);
+                    queue.offer(curr.right);
+                }
+            }
+            int currWidth = end - start + 1;
+            maxWidth = Math.max(maxWidth, currWidth);
+        }
+        return maxWidth;
     }
 }
