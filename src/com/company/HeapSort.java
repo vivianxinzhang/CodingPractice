631b238a10dc41a52ed5c7b737c4035314b11009
Vivian Zhang <vivianxinzhang@gmail.com>
2020-10-12 23:28:48 -0700

    HeapSort

--- src/com/company/HeapSort.java
+++ src/com/company/HeapSort.java
@@ -5,53 +5,67 @@ import java.util.Arrays;
 public class HeapSort {
     public static void main(String[] args) {
         HeapSort s = new HeapSort();
-        int[] array = {4, 4, 1, 1};
+        int[] array = {4, 6, 1, 3};
         System.out.println(Arrays.toString(s.heapsort(array)));
     }
 
     // Time O(nlogn)
     // Space O(1)
     public int[] heapsort(int[] array) {
-        int size = array.length - 1;
         // Step 1: build heap
         // the last non-leaf node will be at size / 2
         // so we will start from this location to heapify the whole array
-        for (int i = size / 2; i >= 0; i--) {
-            heapify(array, i, size);
-        }
-        // array[0] is a root of the heap and is the max element in heap
-        for (int i = size; i > 0; i--) {
-            int tmp = array[0];
-            array[0] = array[i];
-            array[i] = tmp;
-            size = size - 1;
-            heapify(array, 0, size);
+        heapify(array, array.length - 1);
+        for (int endIdx = array.length - 1; endIdx > 0; endIdx--) {
+            swap(array, 0, endIdx);
+            heapify(array, endIdx - 1);
         }
         return array;
     }
 
-    private void heapify(int[] array, int i, int size) {
-        if (size == 0) {
-            return;
+    public void heapify(int[] array, int endIdx) {
+        // Write your solution here
+        int pIdx = (endIdx - 1) / 2;
+        for (int i = pIdx; i >= 0; i--) {
+            percolateDown(array, i, endIdx);
         }
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-        int max = i;
-        // if left child is larger than root
-        if (left <= size && array[left] > array[i]) {
-            max = left;
+    }
+
+    private void percolateUp(int[] array, int i) {
+        while (i > 0) {
+            int parentIdx = (i - 1) / 2;
+            if (array[parentIdx] < array[i]) {
+                swap(array, i, parentIdx);
+                i = parentIdx;
+            } else {
+                break;
+            }
         }
-        // if right child is larger than largest so far
-        if (right <= size && array[right] > array[max]) {
-            max = right;
+    }
+
+    private void percolateDown(int[] array, int i, int endIdx) {
+        while (2 * i + 1 <= endIdx) {
+            int leftIdx = 2 * i + 1;
+            int rightIdx = 2 * i + 2;
+            // smallest one among left child and right child
+            int swapCandidate = leftIdx;
+            // update swapCandidate if right child exist and it is smaller than left child
+            if (rightIdx <= endIdx && array[leftIdx] <= array[rightIdx]) {
+                swapCandidate = rightIdx;
+            }
+            // swap if necessary;
+            if (array[i] < array[swapCandidate]) {
+                swap(array, i, swapCandidate);
+                i = swapCandidate;
+            } else {
+                break;
+            }
         }
-        // if max is not current node, exchange it with max of left or right
-        if (max != i) {
-            int tmp = array[i];
-            array[i] = array[max];
-            array[max] = tmp;
-        }
-        // recursive call
-        heapify(array, max, size - 1);
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Solution {
 
-
 }
 
 
