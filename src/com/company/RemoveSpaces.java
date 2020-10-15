1a5913c7fbd5d664678b6a74a9d70239f059d426
Vivian Zhang <vivianxinzhang@gmail.com>
2020-10-15 10:10:06 -0700

    RemoveSpaces

---
+++ src/com/company/RemoveSpaces.java
@@ -0,0 +1,65 @@
+package com.company;
+import java.util.Arrays;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        RemoveSpaces s = new RemoveSpaces();
+        System.out.println("main");
+        String input = " a";
+        System.out.println(s.removeSpaces(input));
+
+        input = "   I      love MTV   ";
+        System.out.println(s.removeSpaces(input));
+    }
+
+    // Assumption: input is not null
+    // Time O(n)
+    // Space O(n)
+    public String removeSpaces(String input) {
+        // Write your solution here
+        char[] array = input.toCharArray();
+        int slow = 0;
+        int fast = 0;
+        while (fast < array.length) {
+            if ((fast == 0 || array[fast - 1] == ' ') && array[fast] == ' ') {
+                fast++;
+            } else {
+                array[slow++] = array[fast++];
+            }
+        }
+        // post-processing
+        if (slow > 0 && array[slow - 1] == ' ') {
+            slow--;
+        }
+        return new String(array, 0, slow);
+    }
+
+    // Assumption: input is not null
+    // Time O(n)
+    // Space O(n)
+    public String removeSpacesI(String input) {
+        // Write your solution here
+        if (input.isEmpty()) {
+            return input;
+        }
+        char[] array = input.toCharArray();
+        int end = 0;
+        for (int i = 0; i < array.length; i++) {
+            // when we would ignore the current space character:
+            // 1. we ignore all the space characters followed by another space character, so that
+            // if there are several consecutive space characters, only the first one will be remained
+            // 2. we ignore also the space character if it is the first character of the input string
+            // post-processing: it is possible we still have one trailing
+            // space character at the end
+            if (array[i] == ' ' && (i == 0 || array[i - 1] == ' ')) {
+                continue;
+            }
+            array[end++] = array[i];
+        }
+        // post-processing
+        if (end > 0 && array[end - 1] == ' ') {
+            end--;
+        }
+        return new String(array, 0, end);
+    }
+}
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -5,28 +5,5 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("main");
         Solution s = new Solution();
-//        String[] combo = {"a", "a", "b", "b", "b", "b", "c", "c", "c", "d"};
-//        System.out.println(Arrays.toString(combo));
-//        String str = "abc";
-        int[] A = {1, 3, 5};
-        int[] B = {4, 8};
-        System.out.println(s.kthSmallest(A, B, 4));
-    }
-
-    // Time O(m + n)
-    // Space O(1)
-    public List<Integer> common(int[] A, int[] B) {
-        // Write your solution here
-        List<Integer> result = new ArrayList<>();
-        Set<Integer> seen = new HashSet<>();
-        for (int ele : A) {
-            seen.add(ele);
-        }
-        for (int ele : B) {
-            if (seen.contains(ele)) {
-                result.add(ele);
-            }
-        }
-        return result;
     }
 }
