e745e6c62eae4fd1c7cb3c602f396a52eee41401
Vivian Zhang <vivianxinzhang@gmail.com>
2020-10-25 11:58:23 -0700

    update MyHashMap

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -4,11 +4,19 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] array = new int[] {1, 2, 3, 4, 5, 6};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6};
         System.out.println();
     }
-
 }
+
+
+
+
+
+
+
+
+
 
 
 
