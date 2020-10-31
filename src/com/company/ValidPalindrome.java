32e9331f2f84a9f7bdaefce6a277307af2447070
Vivian Zhang <vivianxinzhang@gmail.com>
2020-10-31 14:05:01 -0700

    ValidPalindrome

---
+++ src/com/company/ValidPalindrome.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        ValidPalindrome s = new ValidPalindrome();
+        System.out.println();
+    }
+
+    // Time O(n)
+    // Space O(1)
+    public boolean valid(String input) {
+        // Write your solution here
+        input = input.toLowerCase();
+        return valid(input, 0, input.length() - 1);
+    }
+
+    private boolean valid(String input, int i, int j) {
+        while (i < j) {
+            while (i < j && !alphanumeric(input.charAt(i))) {
+                i++;
+            }
+            while (i < j && !alphanumeric(input.charAt(j))) {
+                j--;
+            }
+            if (input.charAt(i) != input.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private boolean alphanumeric(Character a) {
+        return Character.isDigit(a) || Character.isAlphabetic(a);
+    }
+}
