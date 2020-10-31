1bc7d14eba64a4eeb7c742a9ff8d4f6ffc42df49
Vivian Zhang <vivianxinzhang@gmail.com>
2020-10-31 12:31:52 -0700

    LongestPalindromicSubstring

---
+++ src/com/company/LongestPalindromicSubstring.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        LongestPalindromicSubstring s = new LongestPalindromicSubstring();
+        System.out.println(s.longestPalindrome("abcbcbd"));
+
+        System.out.println(s.longestPalindrome("abbc"));
+    }
+
+    // Time O(n^2)
+    // Space O(1)
+    public String longestPalindrome(String input) {
+        // Write your solution here
+        int maxLength = 0;
+        int start = 0;
+        int globalLeft = 0;
+        int globalRight = 0;
+        for (int i = 1; i < input.length(); i++) {
+            int currLength = 1;
+            // Case 1: palindrome has odd length
+            int left = i;
+            int right = i;
+            while (left >= 0 && right < input.length() && input.charAt(left) == input.charAt(right)) {
+                left--;
+                right++;
+            }
+            currLength = right - left - 1;
+            if (currLength > maxLength) {
+                maxLength = currLength;
+                globalLeft = left + 1;
+                globalRight = right - 1;
+            }
+            // Case 1: palindrome has even length
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < input.length() && input.charAt(left) == input.charAt(right)) {
+                left--;
+                right++;
+            }
+            currLength = right - left - 1;
+            if (currLength > maxLength) {
+                maxLength = currLength;
+                globalLeft = left + 1;
+                globalRight = right - 1;
+            }
+        }
+        return input.substring(globalLeft, globalRight + 1);
+    }
+
+
+    // Method 1: brute force
+    // Time O(n^3)
+    // Space O(1)
+    public String longestPalindromeI(String input) {
+        // Write your solution here
+        if (input == null || input.length() == 0) {
+            return input;
+        }
+        int maxLength = 0;
+        int globalLeft = 0;
+        int globalRight = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i; j < input.length(); j++) {
+                int currLength = j - i + 1;
+                if (currLength > maxLength && isPalindrome(input, i, j)) {
+                    maxLength = currLength;
+                    globalLeft = i;
+                    globalRight = j;
+                }
+            }
+        }
+        return input.substring(globalLeft, globalRight + 1);
+    }
+
+    private boolean isPalindrome(String input, int i, int j) {
+        while (i < j) {
+            if (input.charAt(i) != input.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -4,15 +4,31 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-
-                Solution s = new Solution();
-        String input = "012345";
-        int[][] matrix = new int[][] {{3,4,5}, {3,2,6},{2,2,1}};
+        Solution s = new Solution();
         System.out.println();
     }
 
 
 
 
-
+    public String removeSpaces(String input) {
+        // Write your solution here
+        if (input == null || input.length() == 0) {
+            return input;
+        }
+        char[] array = input.toCharArray();
+        int slow = 0;
+        int fast = 0;
+        while (fast < array.length) {
+            if ((fast == 0 || array[fast - 1] == ' ') && array[fast] == ' ') {
+                fast++;
+            } else {
+                array[slow++] = array[fast++];
+            }
+        }
+        if (slow > 0 && array[slow - 1] == ' ') {
+            slow--;
+        }
+        return new String(array, 0, slow);
+    }
 }
