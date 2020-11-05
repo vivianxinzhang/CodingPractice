3435233c2262f882b85ca063cad390aee462516c
Vivian Zhang <vivianxinzhang@gmail.com>
2020-11-04 22:23:30 -0800

    update MinimumHeightTrees

--- src/com/company/MinimumHeightTrees.java
+++ src/com/company/MinimumHeightTrees.java
@@ -23,9 +23,59 @@ public class MinimumHeightTrees {
         System.out.println(s.findMinHeightTrees(2, edges));
     }
 
+    // Method 2:
+    // Time O(n)
+    // Space O(n)
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        // base cases
+        if (n < 2) {
+            ArrayList<Integer> centroids = new ArrayList<>();
+            for (int i = 0; i < n; i++)
+                centroids.add(i);
+            return centroids;
+        }
+        // Build the graph with the adjacency list
+        ArrayList<Set<Integer>> neighbors = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            neighbors.add(new HashSet<Integer>());
+        for (int[] edge : edges) {
+            Integer start = edge[0], end = edge[1];
+            neighbors.get(start).add(end);
+            neighbors.get(end).add(start);
+        }
+        // Initialize the first layer of leaves
+        ArrayList<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (neighbors.get(i).size() == 1) {
+                leaves.add(i);
+            }
+        }
+        // Trim the leaves until reaching the centroids
+        int remainingNodes = n;
+        while (remainingNodes > 2) {
+            remainingNodes -= leaves.size();
+            ArrayList<Integer> newLeaves = new ArrayList<>();
+            // remove the current leaves along with the edges
+            for (Integer leaf : leaves) {
+                for (Integer neighbor : neighbors.get(leaf)) {
+                    neighbors.get(neighbor).remove(leaf);
+                    // for nodes with only 1 edge are leaf nodes
+                    if (neighbors.get(neighbor).size() == 1) {
+                        newLeaves.add(neighbor);
+                    }
+                }
+            }
+            // prepare for the next round
+            leaves = newLeaves;
+        }
+        // The remaining nodes are the centroids of the graph
+        return leaves;
+    }
+
+    // Method 1: brute force
     // Time O(n(n + edges.length)) -> O(V(V + E))
     // Space O(n) -> O(V)
-    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+    public List<Integer> findMinHeightTreesI(int n, int[][] edges) {
         List<List<Integer>> neighbors = getNeightbors(n, edges);
         List<Integer> result = new ArrayList<>();
         int minHeight = Integer.MAX_VALUE;
