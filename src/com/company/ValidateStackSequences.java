b8f95728f4e0389e0c42ab3b24b4d2f30713c542
Vivian Zhang <vivianxinzhang@gmail.com>
2020-11-04 22:56:05 -0800

    ValidateStackSequences

---
+++ src/com/company/ValidateStackSequences.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.*;
+
+public class ValidateStackSequences {
+    public static void main(String[] args) {
+        ValidateStackSequences s = new ValidateStackSequences();
+        int[] pushed = new int[]{1, 2, 3, 4, 5};
+        int[] popped = new int[]{4, 5, 3, 2, 1};
+        System.out.println(s.validateStackSequences(pushed, popped));
+
+        pushed = new int[]{1, 2, 3, 4, 5};
+        popped = new int[]{4, 3, 5, 1, 2};
+        System.out.println(s.validateStackSequences(pushed, popped));
+    }
+
+    // Time O(n)
+    // Space O(n)
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        // Write your solution here
+        Deque<Integer> stack = new ArrayDeque<>();
+        int i = 0;
+        for (int num : pushed) {
+            stack.offerFirst(num);
+            while (!stack.isEmpty() && stack.peekFirst() == popped[i]) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
