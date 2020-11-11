d6591d6ba817b56ed1f18fcd98c1e1f74cfb24b2
Vivian Zhang <vivianxinzhang@gmail.com>
2020-11-10 23:47:16 -0800

    CompressString

---
+++ src/com/company/CompressString.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class CompressString {
+    public static void main(String[] args) {
+        CompressString s = new CompressString();
+        System.out.println(s.compress("a"));
+
+        System.out.println(s.compress("cccc"));
+
+        System.out.println(s.compress("abbcccdeee"));   // "ab2c3de3"
+    }
+
+    // Time O(n)
+    // Space O(n)
+    public String compress(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+        char[] array = input.toCharArray();
+        return encode(array);
+    }
+
+    private String encode(char[] array) {
+        int slow = 0;
+        int fast = 0;
+        int newLength = 0;
+        while (fast < array.length) {
+            int beginIdx = fast;
+            while (fast < array.length && array[fast] == array[beginIdx]) {
+                fast++;
+            }
+            array[slow++] = array[beginIdx];
+            if (fast - beginIdx != 1) {
+                int digitLength = copyDigits(array, slow, fast - beginIdx);
+                slow += digitLength;
+            }
+        }
+        return new String(array, 0, slow);
+    }
+
+    private int copyDigits(char[] array, int start, int count) {
+        int numOfDigits = 0;
+        for (int i = count; i > 0; i /= 10) {
+            numOfDigits++;
+        }
+        int end = start + numOfDigits - 1;
+        for (int i = end; i >= start; i--) {
+            array[i] = (char)(count % 10 + '0');
+            count /= 10;
+        }
+        return numOfDigits;
+    }
+}
--- src/com/company/CompressStringII.java
+++ src/com/company/CompressStringII.java
@@ -1,6 +1,15 @@
 package com.company;
 
 public class CompressStringII {
+    public static void main(String[] args) {
+        CompressStringII s = new CompressStringII();
+        System.out.println(s.compress("a"));
+
+        System.out.println(s.compress("cccc"));
+
+        System.out.println(s.compress("abbcccdeee"));   // "a1b2c3d1e3"
+    }
+
     // Time O(n)
     // Space O(1)
     public String compress(String input) {
@@ -18,9 +27,11 @@ public class CompressStringII {
         int newLength = 0;
         while (fast < input.length) {
             int begin = fast;
+            // count number of repetitive char
             while (fast < input.length && input[fast] == input[begin]) {
                 fast++;
             }
+            // copy char
             input[slow++] = input[begin];
             if (fast - begin == 1) {
                 newLength += 2;
@@ -34,16 +45,22 @@ public class CompressStringII {
         // Notice: if it is required to do this in place, usually the input array is a
         // sufficient large one, you will not need to allocate a new array.
         char[] result = new char[newLength];
+        // [0, slow) are all processed
+        // when go backward, should start at index slow - 1
         fast = slow - 1;
         slow = newLength - 1;
+        // each iteration copy a pattern
         while (fast >= 0) {
             if (Character.isDigit(input[fast])) {
+                // Case 1: pattern starts with number --> already compressed
+                // copy all numbers
                 while (fast >= 0 && Character.isDigit((input[fast]))) {
                     result[slow--] = input[fast--];
                 }
-            } else {
+            } else {   // Case 2: pattern starts with letter --> only one letter case
                 result[slow--] = '1';
             }
+            // copy letter
             result[slow--] = input[fast--];
         }
         return new String(result);
