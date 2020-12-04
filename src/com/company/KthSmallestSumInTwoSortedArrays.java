671f0aa3ce35db93af10db962a1f16d81439ac0b
Vivian Zhang <vivianxinzhang@gmail.com>
2020-12-03 17:31:29 -0800

    KthSmallestSumInTwoSortedArrays

---
+++ src/com/company/KthSmallestSumInTwoSortedArrays.java
@@ -0,0 +1,74 @@
+package com.company;
+import java.util.*;
+
+public class KthSmallestSumInTwoSortedArrays {
+    public static void main(String[] args) {
+        KthSmallestSumInTwoSortedArrays s = new KthSmallestSumInTwoSortedArrays();
+        int[] A = new int[] {1, 3, 5};
+        int[] B = new int[] {4, 8};
+        System.out.println(s.kthSum(A, B, 1));  // 5
+        System.out.println(s.kthSum(A, B, 2));  // 7
+        System.out.println(s.kthSum(A, B, 3));  // 9
+        System.out.println(s.kthSum(A, B, 5));  // 11
+    }
+
+    // Assumption: 1) m > 0, n > 0
+    //                    2) k >0 and k < m * n
+    //										3) A and B are sorted in ascending order
+    // Approach: Use Best First Search to find the k-th smallest element
+    //                Total possilbe values of  s can be put in an imiginary 2-D array: dimension mn and there could have same values
+    //								Data Structure: Priority Queue -- minHeap
+    //								Initial State: A[0] + B[0]
+    //								Expand: A[i] + B[j]
+    //								Generate: A[i + 1] + B[j] and A[i] + B[j + 1]
+    //								Termination condition: before the k-th element is popped out for expansion
+    //								Deduplication: which node have been generated
+    // Time O(klogk)
+    // Space O(k + m * n)
+    public int kthSum(int[] A, int[] B, int k) {
+        // Best First Search, need a minHeap on the value of each Pair
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>(k, new MyComparator());
+        // all the generated cells will be marked true to avoid being generated more than once
+        boolean[][] visited = new boolean[A.length][B.length];
+        minHeap.offer(new Pair(A, 0, B, 0));
+        visited[0][0] = true;
+        // iterate k - 1 rounds, and Best First Search the smallest k - 1 values
+        for (int q = 0; q < k - 1; q++) {
+            Pair cur = minHeap.poll();
+            // the neighbor Pair will be inserted into the minHeap only if
+            // 1. it is not out of boundary
+            // 2. it has not been generated before
+            if (cur.row + 1 < A.length && !visited[cur.row + 1][cur.col]) {
+                minHeap.offer(new Pair(A, cur.row + 1, B, cur.col));
+                visited[cur.row + 1][cur.col] = true;
+            }
+            if (cur.col + 1 < B.length && !visited[cur.row][cur.col + 1]) {
+                minHeap.offer(new Pair(A, cur.row, B, cur.col + 1));
+                visited[cur.row][cur.col + 1] = true;
+            }
+        }
+        return minHeap.peek().value;
+    }
+
+    static class Pair {
+        int row;
+        int col;
+        int value;
+
+        Pair(int[] A, int i, int[] B, int j) {
+            this.row = i;  // i is the row index in our imiginary 2-D array
+            this.col = j;   // j is the col index in our imiginary 2-D array
+            this.value = A[i] + B[j];
+        }
+    }
+
+    private class MyComparator implements Comparator<Pair> {
+        @Override
+        public int compare(Pair p1, Pair p2) {
+            if (p1.value == p2.value) {
+                return 0;
+            }
+            return p1.value < p2.value ? -1 : 1;
+        }
+    }
+}
--- src/com/company/RestoreIPAddresses.java
+++ src/com/company/RestoreIPAddresses.java
@@ -11,10 +11,6 @@ public class RestoreIPAddresses {
     // valid integer → if there are more than one digit in the integer, the first digit should not be 0
     // 分四层     →  	每层是一个IP的integer
     // 分三个branch   →   每个branch是每个integer可取多少种
-    // 目的是把3个点插入进去把String分成4段：
-    // pruning:
-    // 1. 如果剩余的Number长度不足以给剩下的Integer至少每个数一个digit  或者 远远长于 max可分配的长度(每个都是3位数), 直接返回
-    // 2. 如果上一个被加进path的integer string转换成int后大于255，或者它是一个首位是0且长度大于1的string，则 直接返回
     // Implementation 2:
     // Time O(3^4)
     // Space O(4)
@@ -35,12 +31,12 @@ public class RestoreIPAddresses {
             return;
         }
         // current level
-        // 1 digit ip
+        // 1 digit for current level
         if (offset < ip.length) {
             helper(ip, level + 1, offset + 1, sb.append(ip[offset]).append('.'), result);
             sb.delete(sb.length() - 2, sb.length());
         }
-        // 2 digits ip
+        // 2 digits for current level
         if (offset + 1 < ip.length) {
             char a = ip[offset];
             char b = ip[offset + 1];
@@ -49,7 +45,7 @@ public class RestoreIPAddresses {
                 sb.delete(sb.length() - 3, sb.length());
             }
         }
-        // 3 digits ip
+        // 3 digits for current level
         if (offset + 2 < ip.length) {
             char a = ip[offset];
             char b = ip[offset + 1];
@@ -61,6 +57,10 @@ public class RestoreIPAddresses {
         }
     }
 
+    // 目的是把3个点插入进去把String分成4段：
+    // pruning:
+    // 1. 如果剩余的Number长度不足以给剩下的Integer至少每个数一个digit  或者 远远长于 max可分配的长度(每个都是3位数), 直接返回
+    // 2. 如果上一个被加进path的integer string转换成int后大于255，或者它是一个首位是0且长度大于1的string，则 直接返回
     // Implementation 1:
     // Time O(3^4)
     // Space O(4)
@@ -74,25 +74,26 @@ public class RestoreIPAddresses {
         return result;
     }
 
-    private void DFS(List<String> result,String s,StringBuilder sb,int limit, int index){
+    private void DFS(List<String> result, String s, StringBuilder sb, int level, int index){
         if(index == s.length()){
-            if(limit == 0){
+            if(level == 0){
                 result.add(sb.deleteCharAt(sb.length() - 1).toString());
             }
             return;
         }
-        if(limit == 0){
+        if(level == 0){
             return;
         }
         for(int i = index; i< Math.min(index + 3,s.length()); i++){
             int length = sb.length();
             if(isValid(s.substring(index,i + 1))){
                 sb.append(s.substring(index,i + 1)).append('.');
-                DFS(result,s,sb,limit - 1,i+1);
+                DFS(result,s,sb,level - 1,i+1);
                 sb.setLength(length);
             }
         }
     }
+
     private boolean isValid(String s){
         if(s.charAt(0) == '0'){
             return s.length() == 1;
