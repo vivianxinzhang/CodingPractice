99a7d5ec2623cafe585e4bcc60f1c36ea405191a
Vivian Zhang <vivianxinzhang@gmail.com>
2020-12-28 16:49:28 -0800

    update ladderLength

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,20 +1,11 @@
 package com.company;
-
 import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println();
-
     }
-
-
 }
-
-
-
-
-
 
 
--- src/com/company/WordLadder.java
+++ src/com/company/WordLadder.java
@@ -6,19 +6,131 @@ public class WordLadder {
         WordLadder s = new WordLadder();
          List<String> dict = Arrays.asList("git", "hit", "hog", "hot", "got");
          System.out.println(s.ladderLength("git", "hot", dict));    // 3
-         // git -> hit -> hot
+         // git -> hit -> hot  3
+         System.out.println(s.ladderLengthI("git", "hot", dict));    // 3
+
+        dict = Arrays.asList("bcd","adc","bdd","baa","bbd","cbd","ddb");
+        System.out.println(s.ladderLength("ddb", "cbd", dict));
+        // 0
+        System.out.println(s.ladderLengthI("ddb", "cbd", dict));
     }
 
     // Assumptions:
     // 1. All words have the same length.
     // 2. All words contain only lowercase alphabetic characters.
-    // 3. There is no duplicates in the word list.
+    // 3. There are no duplicates in the word list.
     // 4.The beginWord and endWord are non-empty and are not the same.
-    // Example: start = "git", end = "hot", dictionary = {"git","hit","hog","hot"}
-    // Output: 3
-    // Time O(n * wrod.length)
-    // Space O(n)
+    // Solution 1: BFS
+    // total nodes = b^0 + b^1 + b^2 ... b^d   -> O(b^d)
+    // Time O(word.length * 26 * wordList.length)
+    // Space O(wordList.length)
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = buildSet(wordList);
+        if (!dict.contains(endWord)) {
+            return 0;
+        }
+        Deque<String> queue = new ArrayDeque<>();
+        queue.offer(beginWord);
+        int step = 0;
+        int length = beginWord.length();
+        while (!queue.isEmpty()) {
+            step++;
+            int size = queue.size();
+            for (int count = 0; count < size; count++) {
+                // expand intermediate word
+                char[] word = queue.poll().toCharArray();
+                for (int i = 0; i < length; i++) {
+                    char originChar = word[i];
+                    for (int ch = 'a'; ch <= 'z'; ch++) {
+                        if (ch == originChar) {
+                            continue;
+                        }
+                        word[i] = (char)ch;
+                        String newWord = new String(word);
+                        // find the solution
+                        if (newWord.equals(endWord)) {
+                            return step + 1;
+                        }
+                        // generate neighbor word in the wordList
+                        if (dict.contains(newWord)) {
+                            queue.offer(newWord);
+                            dict.remove(newWord);
+                        }
+                    }
+                    word[i] = originChar;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private Set<String> buildSet(List<String> wordList) {
+        Set<String> set = new HashSet<>();
+        for (String word : wordList) {
+            set.add(word);
+        }
+        return set;
+    }
+
+    // Solution 2: Bidirectional BFS
+    // total nodes = 2(b^0 + b^1 + b^2 ... b^(d/2))   -> O(b^(d/2))
+    // Time O(word.length * 26 * wordList.length)
+    // Space O(wordList.length)
+    public int ladderLengthI(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = buildSet(wordList);
+        if (!dict.contains(endWord)) {
+            return 0;
+        }
+        int step = 0;
+        int length = beginWord.length();
+        Set<String> q1 = new HashSet<>();
+        Set<String> q2 = new HashSet<>();
+        q1.add(beginWord);
+        q2.add(endWord);
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            step++;
+            // always expand the smaller queue first - try to balance
+            if (q1.size() > q2.size()) {
+                swap(q1, q2);
+            }
+            Set<String> q = new HashSet<>();
+            for (String w : q1) {
+                // expand intermediate word
+                char[] word = w.toCharArray();
+                for (int i = 0; i < length; i++) {
+                    char originChar = word[i];
+                    for (int ch = 'a'; ch <= 'z'; ch++) {
+                        if (ch == originChar) {
+                            continue;
+                        }
+                        word[i] = (char)ch;
+                        String newWord = new String(word);
+                        // find the solution
+                        if (q2.contains(newWord)) {
+                            return step + 1;
+                        }
+                        // generate neighbor word in the wordList
+                        if (dict.contains(newWord)) {
+                            q.add(newWord);
+                            dict.remove(newWord);
+                        }
+                    }
+                    word[i] = originChar;
+                }
+                q1 = q;
+            }
+        }
+        return 0;
+    }
+
+    private void swap(Set<String> q1, Set<String> q2) {
+        Set<String> tmp = q1;
+        q1 = q2;
+        q2 = tmp;
+    }
+
+    // another implementation:
+    public int ladderLengthII(String beginWord, String endWord, List<String> wordList) {
         int endIndex = wordList.indexOf(endWord);
         if (endIndex == -1) {
             return 0;
